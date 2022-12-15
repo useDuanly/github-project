5ae96153acc486489ce025d9c14078936cacb667
useDuanly <use.d.v.a.dev@gmail.com>
2022-12-15 17:51:55 +0000

    ok

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 import { Github } from "./components/Github";
 import { UI } from "./components/Ui";
 
-import { client_id, client_secret } from "./config.json";
+let client_id = import.meta.CLIENT_ID,
+  client_secret = import.meta.CLIENT_SECRET;
 const github = new Github(client_id, client_secret);
 const ui = new UI();
 const userForm = document.getElementById("userForm");
